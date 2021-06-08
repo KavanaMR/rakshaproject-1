a2e2a163c61e0e7e81203c12d0816f4060057793
Apoorva <apoorvas546@gmail.com>
2021-06-08 09:55:24 +0530

    “update

--- Raksha/src/app/bike-insurance/bike-insurance.component.ts
+++ Raksha/src/app/bike-insurance/bike-insurance.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bike-insurance',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BikeInsuranceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
-
+  save(){
+    this.router.navigate(['insurance']);
+    
+  }
 }
