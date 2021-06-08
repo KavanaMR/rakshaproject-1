2929941e4f3fa6d6362250b0a5a96c9f8f18e178
Kavana <kavanaraj48@gmail.com>
2021-06-08 13:53:39 +0530

    updates

--- Raksha/src/app/user-dashboard/user-dashboard.component.ts
+++ Raksha/src/app/user-dashboard/user-dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-dashboard',
   templateUrl: './user-dashboard.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
-
+  save(){
+    this.router.navigate(['user-dashboard']);
+    
+  }
 }
